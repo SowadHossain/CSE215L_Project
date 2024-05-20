1b74644c030ea98ced76a4a07dc938b981c63efc
tonoy <mahmudur.tonoy.232@northsouth.edu>
2024-05-21 00:10:58 +0600

    product classes are implemented

---
+++ src/project/Invoice.java
@@ -0,0 +1,67 @@
+package project;
+
+	import java.text.SimpleDateFormat;
+	import java.time.LocalDate;
+	import java.util.ArrayList;
+	import java.util.Date;
+	import java.util.Objects;
+
+	public class Invoice {
+	    private ArrayList<Product> items;
+	    private ArrayList<Product> cart;
+	    private LocalDate date;
+
+	    public Invoice(){
+
+	    }
+	    public String getLocalDateTime (){
+	        LocalDate localDate = LocalDate.now();
+	        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	        return dateFormat.format(localDate);
+	    }
+	    public void addProduct(Product product){
+	        cart.add(product);
+	    }
+	    public void removeProduct(Product product){
+	        cart.remove(product);
+	    }
+	    private double calculatePriceWithoutDiscount(){
+	        double totalPrice = 0;
+	        for (Product o : cart){
+	            totalPrice += o.getPrice();
+	        }
+	        return totalPrice;
+	    }
+	    public boolean isFullHouseDiscountAvailable(){
+
+	        return true;
+	    }
+	    private double calculateDiscountedPrice(){
+	        double discountedPrice = 0,fullHousediscountPrice = 0;
+	        double priceWithoutDiscount = calculatePriceWithoutDiscount();
+	        for (Product o : cart){
+	            discountedPrice += o.getPrice() - o.getDiscount()*o.getPrice()/100;
+	        }
+	        if(isFullHouseDiscountAvailable()){
+	            fullHousediscountPrice = priceWithoutDiscount - 50*priceWithoutDiscount/100;
+	            if(fullHousediscountPrice < discountedPrice)
+	                return fullHousediscountPrice;
+	        }
+	        return discountedPrice;
+	    }
+
+	    public String getInvoice(){
+	        String bill = "";
+	        bill += "Date - " + getLocalDateTime();
+	        for (Product p : cart){
+	            bill += "\nName: " + p.getName() +", Price: " + p.getPrice();
+	        }
+	        bill += "\nTotal Price : " + calculatePriceWithoutDiscount();
+	        bill += "\nPrice after discount: " + calculateDiscountedPrice();
+	        return bill;
+	    }
+
+
+
+	}
+
---
+++ src/project/Movie.java
@@ -0,0 +1,41 @@
+package project;
+
+
+	public class Movie extends StockableProduct{
+
+	    private String director ;
+
+	    public Movie(
+	    		int productId, String name, double price, int yearPublished, String genre, double discount, int numberOfItemsStocked, String director) {
+	        super(productId,  name,  price,  genre,  yearPublished,  discount,  numberOfItemsStocked);
+	        this.director = director;
+	    }
+
+	    public String getDirector() {
+	        return director;
+	    }
+
+	    public void setDirector(String director) {
+	        this.director = director;
+	    }
+
+
+	    public String toString() {
+	        return "Movie [director = " + director + "]";
+	    }
+
+
+	    public String getInfo() {
+	        String string = "Name: " + super.getName();
+	        string += "\nProduct Id: " + super.getProductId();
+	        string += "\nYear Published: " + super.getYearPublished();
+	        string += "\nGenre: " + super.getGenre();
+	        string += "\nDiscount: " + super.getDiscount();
+	        string += "\nPrice: " + super.getPrice();
+	        string += "\nDirector's Name: " + getDirector();
+	        string += "\n";
+
+	        return string;
+	    }
+	}
+
