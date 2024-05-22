a0d98eb49316c270abaa4100d7599abb93b01e26
Sowad <sowadhossain017@gmail.com>
2024-05-23 00:27:38 +0600

    added tonoys code

--- src/Music.java
+++ src/Music.java
@@ -1,2 +1,37 @@
-public class Music {
+
+public class Music extends StockableProduct{
+
+    private String artistName;
+
+    public Music( int productId, String name, double price, int yearPublished, String genre, double discount, int numberOfItemsStocked, String artistName) {
+        super(name, productId,  price,  genre,  yearPublished,  discount,  numberOfItemsStocked);
+        this.artistName = artistName ;
+    }
+
+    public String getName() {
+        return this.artistName;
+    }
+
+    public void setName(String artistName) {
+        this.artistName = artistName;
+    }
+
+
+    public String toString() {
+        return "Music [artistName = " + artistName + "]";
+    }
+
+
+    public String getInfo() {
+        String string = "Name: " + super.getName();
+        string += "\nProduct Id: " + super.getProductId();
+        string += "\nYear Published: " + super.getYearPublished();
+        string += "\nGenre: " + super.getGenre();
+        string += "\nDiscount: " + super.getDiscount();
+        string += "\nPrice: " + super.getPrice();
+        string += "\nArtist's Name: " + getName();
+        string += "\n";
+
+        return string;
+    }
 }
